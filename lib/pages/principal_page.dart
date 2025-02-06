a661e3603b2c8412f9a46d32e9207bf9f795573a
ArthurAndrade1406 <arthur.andrade140606@gmail.com>
2025-02-06 16:00:15 -0300

    Drawer da pagina principal

--- lib/pages/principal_page.dart
+++ lib/pages/principal_page.dart
@@ -55,7 +55,7 @@ class PrincipalPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: const Text('Fenomenos SM'),
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.logout),
@@ -63,6 +63,110 @@ class PrincipalPage extends StatelessWidget {
           ),
         ],
       ),
+      drawer: NavigationDrawer(
+        children: [
+            Text('Fenomenos SM'),
+
+          InkWell( // Compras
+            onTap: () {
+              print('Clicou no Compras');
+            },
+            onLongPress: () {
+              print('Pressionou no Compras');
+            },
+            child: const ListTile(
+              title: Text('Compras'),
+              leading: Icon(Icons.shopping_cart),
+            ),
+          ),
+          InkWell( // Estoque
+            onTap: () {
+              print('Clicou em Estoque');
+            },
+            onLongPress: () {
+              print('Pressionou o Estoque');
+            },
+            child: const ListTile(
+              title: Text('Estoque'),
+              leading: Icon(Icons.inventory_2_outlined),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              print('Clicou no Fornecedor');
+            },
+            onLongPress: () {
+              print('Pressionou o Fornecedor');
+            },            
+            child: const ListTile(
+              title: Text('Fornecedor'),
+              leading: Icon(Icons.fire_truck_outlined),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              print('Funcionarios');
+            },
+            onLongPress: () {
+              print('Pressionou no Funcionarios');
+            },
+            child: const ListTile(
+              title: Text('Funcionarios'),
+              leading: Icon(Icons.group),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              print('Clicou no Movimentacoes Estoque');
+            },
+            onLongPress: () {
+              print('Pressionou o MovimentacoesEstoque');
+            },
+            child: const ListTile(
+              title: Text('Movimentações Estoque'),
+              leading: Icon(Icons.forklift),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              print('Clicou no Produtos');
+            },
+            onLongPress: () {
+              print('Pressionou o Produtos');
+            },
+            child: const ListTile(
+              title: Text('Produtos'),
+              leading: Icon(Icons.sell_outlined),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              print('Clicou no Tipos Estoque');
+            },
+            onLongPress: () {
+              print('Tipos Estoque');
+            },
+            child: const ListTile(
+              title: Text('Tipos Estoque'),
+              leading: Icon(Icons.inventory_outlined),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              print('Clicou no Tipos Movimentacao Estoque');
+            },
+            onLongPress: () {
+              print('Pressionou no Tipos Movimentacao Estoque');
+            },
+            child: const ListTile(
+              title: Text('Tipos Movimentacao Estoque'),
+              leading: Icon(Icons.move_down),
+            ),
+          )
+
+        ],
+      ),
+
       body: FutureBuilder<Map<String, dynamic>?>(
         future: _informacoesUsuarioLogado(),
         builder: (BuildContext context,
@@ -83,7 +187,10 @@ class PrincipalPage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text('ID: ${userInfo['idFuncionario']}'),
+                Text(
+                  'ID: ${userInfo['idFuncionario']}',
+                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
                 Text('Nome: ${userInfo['nome']}'),
                 Text('Email: ${userInfo['email']}'),
                 Text('Setor: ${userInfo['setor']}'),
