1854a57db4aaedb78b418a736cdba1fe7feea7a2
ArthurAndrade1406 <arthur.andrade140606@gmail.com>
2025-02-13 16:42:27 -0300

    botão alterar feito https://trello.com/c/17hoq9fa/11-adicionar-fun%C3%A7%C3%A3o-ao-bot%C3%A3o-editar-da-p%C3%A1gina-principal

--- lib/pages/principal_page.dart
+++ lib/pages/principal_page.dart
@@ -7,8 +7,15 @@ import 'package:front_mercado/pages/produtos/produtos_page.dart';
 import 'package:front_mercado/widgets/drawer.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class PrincipalPage extends StatelessWidget {
+class PrincipalPage extends StatefulWidget {
   const PrincipalPage({super.key});
+
+  @override
+  _PrincipalPageState createState() => _PrincipalPageState();
+}
+
+class _PrincipalPageState extends State<PrincipalPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Future<void> _deslogar(BuildContext context) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -54,6 +61,78 @@ class PrincipalPage extends StatelessWidget {
     return null;
   }
 
+  void _editarFuncionario(BuildContext context, Map<String, dynamic> userInfo) {
+    final TextEditingController nomeController = TextEditingController(text: userInfo['nome']);
+    final TextEditingController emailController = TextEditingController(text: userInfo['email']);
+    final TextEditingController setorController = TextEditingController(text: userInfo['setor']);
+
+     showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Editar Funcionário'),
+        content: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                controller: nomeController,
+                decoration: const InputDecoration(labelText: 'Nome'),
+              ),
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                maxLength: 100,
+                validator: (String? email) {
+                  final RegExp emailRegex = RegExp(
+                      r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$');
+
+                  if (email == null || email.isEmpty) {
+                    return 'Digite um e-mail';
+                  }
+
+                  if (!emailRegex.hasMatch(email)) {
+                    return 'Digite um e-mail válido';
+                  }
+
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: setorController,
+                decoration: const InputDecoration(labelText: 'Setor'),
+              ),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Cancelar'),
+          ),
+          TextButton(
+            onPressed: () async {
+              if (_formKey.currentState!.validate()) {
+                // Atualizar as informações do funcionário
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                userInfo['nome'] = nomeController.text;
+                userInfo['email'] = emailController.text;
+                userInfo['setor'] = setorController.text;
+                await prefs.setString('usuarioLogado', jsonEncode(userInfo));
+                Navigator.of(context).pop();
+              }
+            },
+            child: const Text('Salvar'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,8 +147,12 @@ class PrincipalPage extends StatelessWidget {
       ),
       drawer: const DrawerFenomenos(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // tem q fzr editar
+        onPressed: () async {
+          final userInfo = await _informacoesUsuarioLogado();
+          if (userInfo != null) {
+            _editarFuncionario(context, userInfo);
+
+          }
         },
         child: const Icon(Icons.edit),
       ),
