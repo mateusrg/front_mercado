b05e70ef2d9ecd832fce61bd000f8ae1b8317ae7
mateusrg <mateusrgewehr@gmail.com>
2025-03-13 14:26:42 -0300

    IPs pré-definidos nos params

--- lib/params.dart
+++ lib/params.dart
@@ -1,3 +1,9 @@
 class Params {
-  static const String ipApi = '192.168.0.10';
+  static const String ipApi = casaMateus;
 }
+
+const teleconMateus = '192.168.230.122';
+const teleconLeo = '192.168.230.104';
+const casaMateus = '192.168.0.10';
+const casaLeo = '192.168.1.15';
+const arthur = 'localhost';
